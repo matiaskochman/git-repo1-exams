680b9d8fa6a66df936960200a0592d4a7e5fb0d2
Sébastien David <sebastien.david.1983@gmail.com>
2013-05-10 16:34:39 +0200

    exams-ui routes

--- exams-ui/client/js/main.js
+++ exams-ui/client/js/main.js
@@ -17,15 +17,55 @@ angular.module('exams.directive', []).directive('ngReady', function () {
     };
 });
 
-// angular app
-
-com.exams.App = angular.module('exams', ['exams.directive']);
-
 // angular controller
 
-com.exams.MainCtrl = function ($scope) {
+com.exams.MainCtrl = function ($scope, $location) {
     $scope.preventDefault = function ($event) {
         $event.preventDefault();
     }
+
+    if ($location.path().length === 0) {
+        $location.path('/home');
+    }
 };
-com.exams.MainCtrl.$inject = ['$scope'];
+com.exams.MainCtrl.$inject = ['$scope', '$location'];
+
+com.exams.HomeCtrl = function ($scope) {
+    console.log('HomeCtrl');
+};
+com.exams.HomeCtrl.$inject = ['$scope'];
+
+com.exams.CreateExamsCtrl = function ($scope) {
+    console.log('CreateExamsCtrl');
+};
+com.exams.CreateExamsCtrl.$inject = ['$scope'];
+
+com.exams.ListExamsCtrl = function ($scope) {
+    console.log('ListExamsCtrl');
+};
+com.exams.ListExamsCtrl.$inject = ['$scope'];
+
+// angular app
+
+com.exams.App = angular.module('exams', ['exams.directive'], 
+    ['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+        $routeProvider.when('/home', {
+            templateUrl: '/html/home.html',
+            controller: com.exams.HomeCtrl
+        });
+        
+        $routeProvider.when('/exams/create', {
+            templateUrl: '/html/exams-create.html',
+            controller: com.exams.CreateExamsCtrl
+        });
+
+        $routeProvider.when('/exams/list', {
+            templateUrl: '/html/exams-list.html',
+            controller: com.exams.ListExamsCtrl
+        });
+
+        $locationProvider.html5Mode(false);
+        $locationProvider.hashPrefix('!');
+    }]
+);
