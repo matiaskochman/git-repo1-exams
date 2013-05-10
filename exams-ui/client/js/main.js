7b7cec8e10700027930e7220bcf6639d5a2db34c
Sébastien David <sebastien.david.1983@gmail.com>
2013-05-10 15:35:54 +0200

    exams-ui home page

--- exams-ui/client/js/main.js
+++ exams-ui/client/js/main.js
@@ -5,15 +5,27 @@ window.com ? 0 : window.com = {}, com.exams ? 0 : com.exams = {};
 
 // angular service
 
+// angular directive
 
+angular.module('exams.directive', []).directive('ngReady', function () {
+    return {
+        link: function () {
+            $(document).ready(function () {
+                $(document).foundation();
+            });
+        }
+    };
+});
 
 // angular app
 
-com.exams.App = angular.module('exams', []);
+com.exams.App = angular.module('exams', ['exams.directive']);
 
 // angular controller
 
 com.exams.MainCtrl = function ($scope) {
-    
+    $scope.preventDefault = function ($event) {
+        $event.preventDefault();
+    }
 };
 com.exams.MainCtrl.$inject = ['$scope'];
