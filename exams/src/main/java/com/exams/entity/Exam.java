a38f1731ce794ff08c32e244cf8b69ca5eb0cf09
matias <matiaskochman@gmail.com>
2013-06-25 11:04:12 +0200

    @jsonProperty

--- exams/src/main/java/com/exams/entity/Exam.java
+++ exams/src/main/java/com/exams/entity/Exam.java
@@ -2,13 +2,17 @@ package com.exams.entity;
 
 import java.util.HashSet;
 import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
+
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.json.RooJson;
 import org.springframework.roo.addon.tostring.RooToString;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @RooJavaBean
 @RooToString
@@ -20,5 +24,6 @@ public class Exam {
     private String description;
 
     @ManyToMany(cascade = CascadeType.ALL)
+    @JsonProperty
     private Set<Question> questionsList = new HashSet<Question>();
 }
